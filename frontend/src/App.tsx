ca6da18650425bde2c9462d7d2ee2db807e8fbd2
aishwarya951995@gmail.com <aishwaryan@workz.com>
2023-09-27 21:44:44 +0400

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+   <header>TS MERN AMAZONA</header>
+    <main>
+        
+    </main>
+    <footer>All rights reserved</footer>
+    </div>
+  )
+}
+
+export default App
