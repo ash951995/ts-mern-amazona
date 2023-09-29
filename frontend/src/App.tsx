4f1c5d3ab34ec7d3698b448054163f705f17963d
aishwarya951995@gmail.com <aishwarya951995@gmail.com>
2023-09-29 12:03:16 +0400

    Add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./index.css";
-import { sampleProducts } from "./data";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -22,19 +22,7 @@ function App() {
       </header>
       <main>
         <Container className="mt-3">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image"
-                />
-                <h2>{product.name}</h2>
-                <p>${product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet/>
         </Container>
       </main>
       <footer>
