4cfb34932071c33003ad20622c23a844a612822c
aishwarya951995@gmail.com <aishwarya951995@gmail.com>
2023-09-29 11:30:08 +0400

    Add bootstrap.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,47 @@
-
-
-import './App.css'
-import { sampleProducts } from './data'
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import "./index.css";
+import { sampleProducts } from "./data";
 
 function App() {
-  
-
   return (
-    
-      <div>
-        <header>TS Amazona</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Container>
+            <Navbar.Brand>tsamazona</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-      {sampleProducts.map((product) => (<li key={product.slug} >
-        <img src={product.image} alt={product.name} className="product-image"/>
-        <h2>{product.name}</h2>
-        <p>${product.price}</p>
-                </li>
-                )
-            ) }
-    </main>
-      <footer>All rights reserved.</footer>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved.</div>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
