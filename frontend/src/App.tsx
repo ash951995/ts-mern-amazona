a51518ab6006a2881c911ce2eabc48aca48d297d
aishwarya951995@gmail.com <aishwarya951995@gmail.com>
2023-10-07 13:08:39 +0400

    connect mongodb and add to cart functionality.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 
 import "./index.css";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { useContext, useEffect } from 'react'
-import { Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { Store } from './Store'
 
 function App() {
     const {
-        state: { mode },
+        state: { mode,cart },
         dispatch,
       } = useContext(Store)
     
@@ -30,9 +30,14 @@ function App() {
           <Button variant={mode} onClick={switchModeHandler}>
               <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
             </Button>
-            <a href="/cart" className="nav-link">
+            <Link to="/cart" className="nav-link">
               Cart
-            </a>
+              {cart.cartItems.length > 0 && (
+                <Badge pill bg="danger">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </Badge>
+              )}
+            </Link>
             <a href="/signin" className="nav-link">
               Sign In
             </a>
